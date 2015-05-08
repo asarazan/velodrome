09dd0f5ca84de95eb7fae0421bb2616a15658032
Aaron Sarazan <aaron@levelmoney.com>
2015-05-08 10:50:32 -0700

    License headers

--- velodrome/src/main/java/com/levelmoney/velodrome/Velodrome.java
+++ velodrome/src/main/java/com/levelmoney/velodrome/Velodrome.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2015 Level Money, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.levelmoney.velodrome;
 
 import android.app.Activity;
@@ -8,9 +24,6 @@ import com.levelmoney.velodrome.annotations.HandleResult;
 import java.lang.reflect.Field;
 
 /**
- * Created by Aaron Sarazan on 4/26/15
- * Copyright(c) 2015 Level, Inc.
- *
  * It handles lots of lifecycle stuff. Get it?
  * Eh maybe Tre can come up with a better name.
  *
